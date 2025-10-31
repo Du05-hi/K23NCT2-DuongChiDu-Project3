94edb32582eefbe7d3151207a4ee6a87725f02d1
unknown <duongchidu30072005@gmail.com>
2025-10-31 14:16:21 +0700

    Day03

---
+++ k23cnt2-duongchidu-day03/k23cnt2-duongchidu-day03/src/main/java/k23cnt2_duongchidu_day03/K23cnt2DuongChiduDay03Application.java
@@ -0,0 +1,12 @@
+package k23cnt2_duongchidu_day03;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K23cnt2DuongChiduDay03Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K23cnt2DuongChiduDay03Application.class, args);
+    }
+}
