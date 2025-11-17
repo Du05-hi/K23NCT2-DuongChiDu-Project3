0afce474e0e6d8807a13c821ccd65f97eb41dd2c
unknown <duongchidu30072005@gmail.com>
2025-11-17 12:21:13 +0700

    Day07

---
+++ k23cnt2-duongchidu-day07/k23cnt2-duongchidu-day07/src/main/java/k23cnt2_duongchidu_day07/K23cnt2DuongchiduDay07Application.java
@@ -0,0 +1,12 @@
+package k23cnt2_duongchidu_day07;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class K23cnt2DuongchiduDay07Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(K23cnt2DuongchiduDay07Application.class, args);
+    }
+}
---
+++ k23cnt2-duongchidu-day07/k23cnt2-duongchidu-day07/src/main/java/k23cnt2_duongchidu_day07/repository/ProductRepository.java
@@ -0,0 +1,8 @@
+package k23cnt2_duongchidu_day07.repository;
+
+import k23cnt2_duongchidu_day07.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+
+}
